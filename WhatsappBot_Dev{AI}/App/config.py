002a53b6b567f794b21ebded2121ce76545c0a65
Josue-py <josuemarmolejosmendez1@outlook.com>
2024-09-09 12:25:26 -0400

    Update config.py
    i do a fatal error

--- WhatsappBot_Dev{AI}/App/config.py
+++ WhatsappBot_Dev{AI}/App/config.py
@@ -1,8 +1,8 @@
 ACCES_TOKEN='YOUR_ACCES_TOKEN_HERE_PLS'
-PHONE_NUMBER_ID=''
+PHONE_NUMBER_ID='YOUR_PHONE_NUMBER_ID'
 
 # Other config values :)
 API_BASE_URL = f'https://graph.facebook.com/v20.0/'
 
 # Example: Add your OpenAI key here as well if needed
-OPENAI_API_KEY ='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'
+OPENAI_API_KEY ='YOUR_OPENAI_KEY'
